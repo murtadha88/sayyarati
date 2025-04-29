5415e3ad687188dd07c693a806490ff873776902
Murtadha Almoamen <murtadhaalmoamen8@gmail.com>
2025-04-29 18:58:51 +0300

    sign up completed

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,23 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.views.generic import ListView, DetailView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponse
 
 def home(request):
     return HttpResponse('<h1>Hello ᓚᘏᗢ</h1>')
+
+def signup(request):
+    error_message = ''
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            error_message = 'Invalid sign up - try again'
+    form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'signup.html', context)
+
